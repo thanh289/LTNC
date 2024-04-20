1524dc60a2711d4fc0b5c86500111f11dcf4d2cc
thanh289 <tranduythanh2809@gmail.com>
2024-04-20 21:49:39 +0700

    add

---
+++ BT09/Phần D/check.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void a(int *p) { *p++; }
+void b(int *p) { (*p)++; }
+void c(int p) { p++; }
+
+	int main()
+	{
+	    // int *p = new int(5);
+	    // b(p); 
+        // cout<<*p<<endl;
+        // a(p); 
+        // cout<<*p<<endl;
+        // c(*p);
+        // cout<<*p<<endl;
+
+        // int a=5, *p1, *p2;
+        // p1 = new int(10);
+        // p2 = new int(20);
+        // *p1 = a; p2 = &a; a++;
+        // cout<<*p1<<" "<<*p2<<endl;
+	}
